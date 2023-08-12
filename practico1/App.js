a3381972a9995f6fb73bd28f7028cb9a5a7c27c1
Marcos Cavichioli <mcavichi@gmail.com>
2023-08-12 18:51:16 -0300

    Validacion de los operando

--- practico1/App.js
+++ practico1/App.js
@@ -3,25 +3,57 @@ let numero2 = document.getElementById("numero2");
 let operacion = document.getElementById("operacion");
 let resultado = document.getElementById("resultado");
 
-function calcular() {
-let valor1 = numero1.value;
-let valor2 = numero2.value;
-let operador = operacion.value;
+function validarInput() {
+    do {
+        if (isNaN(parseInt(numero1.value)) || isNaN(parseInt(numero2.value))) {
+            alert('Ingrese un valor en todos los campos!');
+        }
+        else break;
+    } while (!isNaN);
+};
 
-switch (operador) {
-    case "suma":
-        resultado.innerHTML = 'El resultado es ' + (Number(valor1) + Number(valor2));
-    break;
-    case "resta":
-        resultado.innerHTML = 'El resultado es ' + (Number(valor1) - Number(valor2));
-    break;
-    case "multiplicacion":
-        resultado.innerHTML = 'El resultado es ' + valor1 * valor2;
-    break;
-    case "division":
-        resultado.innerHTML = 'El resultado es ' + valor1 / valor2;
-    break;
-}
-}
+
+function calcular() {
+    let valor1 = numero1.value;
+    let valor2 = numero2.value;
+    let operador = operacion.value;
+
+    validarInput();
+
+    switch (operador) {
+        case "suma":
+            if((numero1.value.length === 0) || (numero2.value.length === 0)) {
+                alert ('No se puede realizar la suma.');
+            } else {
+                resultado.innerHTML = 'El resultado es ' + (Number(valor1) + Number(valor2));
+            }
+        break;
+        case "resta":
+            if((numero1.value.length === 0) || (numero2.value.length === 0)) {
+                alert ('No se puede realizar la resta.');
+            } else {
+            resultado.innerHTML = 'El resultado es ' + (Number(valor1) - Number(valor2));
+            }
+        break;
+        case "multiplicacion":
+            if((numero1.value.length === 0) || (numero2.value.length === 0)) {
+                alert ('No se puede realizar la multiplicacion.');
+            } else {
+            resultado.innerHTML = 'El resultado es ' + valor1 * valor2;
+            }
+        break;
+        case "division":
+            if((numero1.value.length === 0) || (numero2.value.length === 0)) {
+                alert ('No se puede realizar la division.');
+            } else if (valor2 === '0') {
+                alert('No se puede dividir por cero. Seleccione otro número.');
+            } else {
+                resultado.innerHTML = 'El resultado es ' + valor1 / valor2;    
+            }
+        break;
+    }
+};
 
 document.getElementById("calcular").addEventListener("click", calcular);
+
+
