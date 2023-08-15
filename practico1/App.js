c57758e47ee629ace9372652def4cac875f9ab56
Marcos Cavichioli <mcavichi@gmail.com>
2023-08-15 19:38:23 -0300

    Mejoras, personalización y comentarios explicativos

--- practico1/App.js
+++ practico1/App.js
@@ -1,8 +1,10 @@
+// Acceso a los Id del Document.
 let numero1 = document.getElementById("numero1");
 let numero2 = document.getElementById("numero2");
 let operacion = document.getElementById("operacion");
 let resultado = document.getElementById("resultado");
 
+// Validando ingreso de numeros en los input.
 function validarInput() {
     do {
         if (isNaN(parseInt(numero1.value)) || isNaN(parseInt(numero2.value))) {
@@ -12,48 +14,73 @@ function validarInput() {
     } while (!isNaN);
 };
 
-
+// Función que realiza los cálculos de las diferentes operación.
 function calcular() {
     let valor1 = numero1.value;
     let valor2 = numero2.value;
     let operador = operacion.value;
-
+    let extremo = 9999999999;
     validarInput();
-
     switch (operador) {
-        case "suma":
+        case "suma": 
+            let suma = Number(valor1) + Number(valor2);
             if((numero1.value.length === 0) || (numero2.value.length === 0)) {
-                alert ('No se puede realizar la suma.');
+                alert ('No se puede realizar la suma.'); 
+            } else if (suma > extremo && suma.toString().length > 10) {
+                alert('Error! El numero es muy grande!')
+            } else if(suma < -extremo && suma.toString().length < 10) {
+                alert('Error! El numero es muy chico!')
             } else {
-                resultado.innerHTML = 'El resultado es ' + (Number(valor1) + Number(valor2));
+                resultado.innerHTML = 'El resultado es ' + suma;
             }
         break;
         case "resta":
+            let resta = Number(valor1) - Number(valor2);
             if((numero1.value.length === 0) || (numero2.value.length === 0)) {
                 alert ('No se puede realizar la resta.');
+            } else if (resta > extremo && resta.toString().length > 10) {
+                alert('Error! El numero es muy grande!')
+            } else if(resta < -extremo && resta.toString().length < 10) {
+                alert('Error! El numero es muy chico!')
             } else {
-            resultado.innerHTML = 'El resultado es ' + (Number(valor1) - Number(valor2));
+            resultado.innerHTML = 'El resultado es ' + resta;
             }
         break;
         case "multiplicacion":
+            let multiplicacion = valor1 * valor2;
             if((numero1.value.length === 0) || (numero2.value.length === 0)) {
                 alert ('No se puede realizar la multiplicacion.');
+            } else if (multiplicacion > extremo && multiplicacion.toString().length > 10) {
+                alert('Error! El numero es muy grande!')
+            } else if(multiplicacion < -extremo && multiplicacion.toString().length < 10) {
+                alert('Error! El numero es muy chico!')
             } else {
-            resultado.innerHTML = 'El resultado es ' + valor1 * valor2;
+            resultado.innerHTML = 'El resultado es ' + multiplicacion;
             }
         break;
         case "division":
+            let division = valor1 / valor2;
             if((numero1.value.length === 0) || (numero2.value.length === 0)) {
                 alert ('No se puede realizar la division.');
             } else if (valor2 === '0') {
                 alert('No se puede dividir por cero. Seleccione otro número.');
+            } else if (division > extremo && division.toString().length > 10) {
+                alert('Error! El numero es muy grande!')
+            } else if(division < -extremo && division.toString().length < 10) {
+                alert('Error! El numero es muy chico!')    
             } else {
-                resultado.innerHTML = 'El resultado es ' + valor1 / valor2;    
+                resultado.innerHTML = 'El resultado es ' + division;    
             }
         break;
-    }
+    };
 };
 
+// Funcion que resetea el formulario.
+function resetearFormulario() {
+    document.getElementById("formulario").reset();
+    document.getElementById("resultado").innerHTML = '';
+};
+
+//Eventos.
 document.getElementById("calcular").addEventListener("click", calcular);
-
-
+document.getElementById("reset").addEventListener('click', resetearFormulario);
